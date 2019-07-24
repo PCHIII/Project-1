a3d5eab65389bc48b088d677cfae0506dcf0e8ae
LindoK <khavin.lindo@gmail.com>
2019-07-24 17:27:33 -0400

    2nd Update

--- assets/main.js
+++ assets/main.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-
-
-var movieURL = "";
-var bookURL = "";
-
-
-$(document).ready(function () {
-
-$("#search").on("click", function(e) {
-   e.preventDefault();
-    
-   var input = $("#input").val().trim();
-   console.log(input);
-   movieURL = "https://www.omdbapi.com/?t="+input+"&apikey=trilogy";
-   bookURL = "https://www.googleapis.com/books/v1/volumes?q="+input;
-
-    $.ajax({
-        url: movieURL,
-        method: "GET"
-    }).then(function(res) {
-         console.log("------MOVIE OBJECT-----------")
-         console.log(res);
-    });
-
-    $.ajax({
-        url: bookURL,
-        method: "GET"
-    }).then(function(res) {
-         console.log("------BOOK OBJECT------------")
-         console.log(res);
-    });
-});
-
-
-});
-=======
 $(document).ready(function() {
                 
     
@@ -45,4 +8,3 @@ $(document).ready(function() {
     
     
     });
->>>>>>> c8417f9e689a1330c2933a03f7eed50a7cc9d677
